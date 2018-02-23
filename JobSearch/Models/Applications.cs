e4de10e25541b27cc70676ab0aa55035bb4f74ba
bryanhmiller <bryankalos@hotmail.com>
2018-02-22 21:50:11 -0600

    Began working on data model for applications, ran short on time.

---
+++ JobSearch/Models/Applications.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobSearch.Models
+{
+    public class Applications
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public 
+    }
+
+    public enum Status
+    {
+        Contacted,
+        Applied,
+        Interview
+    }
+}
